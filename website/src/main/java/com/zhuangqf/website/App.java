c81eaed9501a4ba8ae7c7477c8f58223dc65aabe
zhuang-sysu <zhuangqinfa@outlook.com>
2018-01-09 22:24:26 +0800

    add Backstage

---
+++ website/src/main/java/com/zhuangqf/website/App.java
@@ -0,0 +1,18 @@
+package com.zhuangqf.website;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhuangqf
+ * @date 2018/1/7
+ */
+
+@SpringBootApplication
+public class App {
+
+    public static void main(String[] args){
+        SpringApplication.run(App.class,args);
+    }
+
+}
